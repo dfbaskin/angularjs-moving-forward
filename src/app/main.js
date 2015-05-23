ebc4db4506292ed0bd7960e0be11f5114292579e
Dave Baskin <dbaskin@wintellect.com>
2015-05-23 15:29:56 -0400

    Updating content.

--- src/app/main.js
+++ src/app/main.js
@@ -1,7 +1,11 @@
 
 import fontAwesome from 'font-awesome/css/font-awesome.css!';
+import $ from 'foundation/js/vendor/jquery';
+import foundation from 'foundation/js/foundation';
 import initializeCodeFormatting from './codeFormatting';
 import presentation from './presentation';
+
+$(document).foundation();
 
 initializeCodeFormatting();
 
--- src/app/presentation.js
+++ src/app/presentation.js
@@ -10,7 +10,7 @@ class Presentation {
 
     constructor() {
         this.deck = bespoke.from('article', [
-            bespokeScale('zoom'),
+            //bespokeScale('zoom'),
             bespokeClasses(),
             bespokeKeys(),
             bespokeHash(),
