f0a6bd8cc070a8f08ca1a67323cd4e62694d1011
Dave Baskin <dbaskin@wintellect.com>
2015-05-25 16:19:09 -0400

    Adding aria content.

---
+++ src/examples/aria/ng-aria.js
@@ -0,0 +1,48 @@
+
+import "bootstrap/css/bootstrap.css!";
+import angular from 'angular';
+import ngMessages from 'angular-messages';
+import ngAria from 'angular-aria';
+
+class ExampleCtrl {
+
+    constructor() {
+        this.userName = "Joe";
+        this.emailAddr = "test@mail.com";
+    }
+
+    submitForm() {
+        // submitted ...
+    }
+
+}
+
+function AsyncUserNameValidator($q, $timeout) {
+
+    return {
+        require: 'ngModel',
+        link: function($scope, element, attrs, ngModel) {
+            ngModel.$asyncValidators.available = function(userName) {
+                // Simulate a call to a back-end service
+                return $timeout(() =>
+                    (userName.toLowerCase() !== "user") ?
+                        $q.when(true) :
+                        $q.reject(),
+                2000);
+            };
+        }
+    }
+}
+
+angular
+    .module('exampleApp', ['ngMessages', 'ngAria'])
+    .directive('userNameExists', [
+        '$q',
+        '$timeout',
+        AsyncUserNameValidator
+    ])
+    .controller('exampleCtrl', [
+        ExampleCtrl
+    ]);
+
+angular.bootstrap(document, ['exampleApp']);
